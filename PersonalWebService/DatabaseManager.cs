91480fdbd93964eb101cdeb7638f487198c0ee32
Ashley Gibson <ash.gibson@hotmail.co.uk>
2019-05-17 19:48:45 +0100

    Initial Commit

---
+++ PersonalWebService/DatabaseManager.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace PersonalWebService
+{
+    public class DatabaseManager
+    {
+        #region Members
+
+        private readonly string _databaseConnectionString = ConfigurationManager.ConnectionStrings["WebServiceDataConnectionString"].ConnectionString;
+
+        private string testOutputString = "";
+        private const string outputStringValidator = "hello";
+
+        #endregion
+
+        #region Methods
+
+        public string GetDatabaseConnectionString()
+        {
+            return _databaseConnectionString;
+        }
+        
+        public bool TestConnectionString()
+        {
+            SqlConnection sqlConnection = new SqlConnection(_databaseConnectionString);
+
+            try
+            {
+                sqlConnection.Open();
+                sqlConnection.Close();
+
+                return true;
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("SQL Error: " + err.InnerException);
+
+                sqlConnection.Close();
+
+                return false;
+            }
+        }
+
+        public string GetTestData()
+        {
+            SqlConnection sqlConnection = new SqlConnection(_databaseConnectionString);
+
+            try
+            {
+                string sqlCommandText = "SELECT * FROM TestTable1";
+                            
+                sqlConnection.Open();
+
+                SqlCommand sqlCommand = new SqlCommand(sqlCommandText, sqlConnection);
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                int i = 0;
+                while(sqlDataReader.Read())
+                {
+                    testOutputString = testOutputString + sqlDataReader.GetValue(i);
+                    i++;
+                }
+
+                sqlConnection.Close();
+
+                return testOutputString.Replace(" ", String.Empty);           
+            }
+            catch(Exception err)
+            {
+                sqlConnection.Close();
+
+                return "SQL Error: " + err.Message;                
+            }            
+        }
+
+        #endregion
+    }
+}
---
+++ PersonalWebService/HelloWorldWebService.svc.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+
+namespace PersonalWebService
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class HelloWorldWebService
+    {
+        #region Members
+
+        private static readonly DatabaseManager dbManager = new DatabaseManager();
+        private static readonly string connectionString = dbManager.GetDatabaseConnectionString();
+        
+        #endregion
+
+        #region Methods
+
+        // GET
+        [WebGet(UriTemplate = "/GetTestData")]
+        public String GetTestData()
+        {
+            return dbManager.GetTestData();
+        }
+
+        #endregion
+               
+        #region TutorialMembers
+
+        private static List<string> list = new List<string>(new String[] { "Arrays", "Queues", "Stacks" });
+
+        #endregion
+
+        #region TutorialMethods
+
+        // GET
+        [WebGet(UriTemplate="/Tutorial")]
+        public String GetAllTutorials()
+        {
+            int count = list.Count;
+            String TutorialList = "";
+            for (int i = 0; i < count; i++)
+                TutorialList = TutorialList + list[i] + ",";
+            return TutorialList;            
+        }
+
+        // GET
+        [WebGet(UriTemplate="/Tutorial/{Tutorialid}")]
+        public String GetTutorialByID(String Tutorialid)
+        {
+            int pid;
+            Int32.TryParse(Tutorialid, out pid);
+            return list[pid];
+        }
+
+        // POST
+        [WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json, UriTemplate ="/Tutorial", ResponseFormat = WebMessageFormat.Json, BodyStyle =WebMessageBodyStyle.Wrapped)]
+        public void AddTutorial(String str)
+        {
+            list.Add(str);
+        }
+
+        // DELETE
+        [WebInvoke(Method = "DELETE", RequestFormat =WebMessageFormat.Json, UriTemplate ="/Tutorial/{Tutorialid}", ResponseFormat =WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped)]
+        public void DeleteTutorial(String Tutorialid)
+        {
+            int pid;
+            Int32.TryParse(Tutorialid, out pid);
+            list.RemoveAt(pid);
+        }
+
+        [OperationContract]
+        public void DoWork()
+        {
+            // Add your operation implementation here
+            return;
+        }
+
+        #endregion
+    }
+}
