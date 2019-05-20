25b2e37ebe62bd2278d0b333a97afb26f823b02e
Ashley Gibson <ash.gibson@hotmail.co.uk>
2019-05-20 21:46:53 +0100

    Added System.Net.Http NuGet Package and begun development of Visual Studio Blog RSS Feed Web Service

--- PersonalWebService/PersonalWebService/HelloWorldWebService.svc.cs
+++ PersonalWebService/PersonalWebService/HelloWorldWebService.svc.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ServiceModel;
 using System.ServiceModel.Activation;
 using System.ServiceModel.Web;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace PersonalWebService
 {
@@ -14,7 +16,12 @@ namespace PersonalWebService
 
         private static readonly DatabaseManager dbManager = new DatabaseManager();
         private static readonly string connectionString = dbManager.GetDatabaseConnectionString();
-        
+
+        public class DataObject
+        {
+            public string Name { get; set; }
+        }
+
         #endregion
 
         #region Methods
@@ -26,8 +33,45 @@ namespace PersonalWebService
             return dbManager.GetTestData();
         }
 
+        // GET
+        [WebGet(UriTemplate = "/VisualStudioBlog")]
+        public String GetVisualStudioBlogFeed()
+        {
+            const string URL = "https://devblogs.microsoft.com/visualstudio/feed/";
+            string urlParameters = "";
+
+            HttpClient client = new HttpClient();            
+            client.BaseAddress = new Uri(URL);
+
+            // Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/rss+xml"));
+
+            // List data response.
+            HttpResponseMessage response = client.GetAsync(urlParameters).Result;  // Blocking call! Program will wait here until a response is received or a timeout occurs.
+            if (response.IsSuccessStatusCode)
+            {
+                // Parse the response body
+                var dataObjects = response.Content.ReadAsStringAsync().Result;  //Make sure to add a reference to System.Net.Http.Formatting.dll
+                foreach (var d in dataObjects)
+                {
+                    Console.WriteLine("{0}", d);
+                }
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
+
+            //Make any other calls using HttpClient here.
+
+            //Dispose once all HttpClient calls are complete. This is not necessary if the containing object will be disposed of; for example in this case the HttpClient instance will be disposed automatically when the application terminates so the following call is superfluous.
+            client.Dispose();
+
+            return "";
+        }
+
         #endregion
-               
+
         #region TutorialMembers
 
         private static List<string> list = new List<string>(new String[] { "Arrays", "Queues", "Stacks" });
