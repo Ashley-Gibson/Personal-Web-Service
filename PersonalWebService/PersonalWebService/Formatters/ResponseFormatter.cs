b45aac3239ad59d1181ff34aa24e74b1feb62933
Ashley Gibson <ash.gibson@hotmail.co.uk>
2019-05-23 19:46:22 +0100

    Added Website Project

---
+++ PersonalWebService/PersonalWebService/Formatters/ResponseFormatter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace PersonalWebService
+{
+    public class ResponseFormatter
+    {
+        public static string FormatVisualStudioBlogResponse(List<XMLParser.Item> parsedResponse)
+        {
+            string html = "";
+
+            foreach (var item in parsedResponse)
+            {
+                // Format Parsed XML
+                html += item.title + "\n" 
+                    + item.link + "\n"
+                    + item.pubDate + "\n"
+                    + item.creator + "\n"
+                    + item.category + "\n"
+                    + item.description + "\n"
+                    + item.encodedContent + "\n"
+                    + item.other + "\n\n\n";           
+            }
+
+            return html;
+        }
+    }
+}
