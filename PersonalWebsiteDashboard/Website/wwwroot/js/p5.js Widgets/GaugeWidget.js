773c3a07d25431065ab095b978ef9d5577517fd5
Ashley Gibson <ash.gibson@hotmail.co.uk>
2019-05-28 21:56:30 +0100

    Begun Dashboard Widget development using p5.js

---
+++ PersonalWebsiteDashboard/Website/wwwroot/js/p5.js Widgets/GaugeWidget.js
@@ -0,0 +1,37 @@
+﻿// Global Variables
+let progressX = 16.5;
+let progressY = 40;
+
+function setup() {
+    createCanvas(100, 40);
+
+    drawEmptyCurvedTube();
+
+    frameRate(30);
+}
+
+function draw() {
+    updateProgress(progressX, progressY);
+
+    progressX = progressX + 0.1;
+    progressY = progressY - 0.1;
+}
+
+function drawEmptyCurvedTube() {
+    // Semi Circle
+    noFill();
+    stroke("black");
+    ellipse(50, 40, 90, 70);
+
+    // Hole in Semi Circle
+    fill("white");
+    stroke("black");
+    ellipse(50, 40, 45, 35);
+}
+
+function updateProgress(progressX, progressY) {
+    // Draw First Circle to start loading animation progress
+    fill("black");
+    noStroke();
+    ellipse(progressX, progressY, 20, 20);
+}
