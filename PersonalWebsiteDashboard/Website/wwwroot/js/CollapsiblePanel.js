68b016eb33ac2d9835f4d18fc4b3c3a714f15fcd
Ashley Gibson <AGibson@quba.co.uk>
2019-05-30 15:00:26 +0100

    Better styling and added the second NestedCircles widget

--- PersonalWebsiteDashboard/Website/wwwroot/js/CollapsiblePanel.js
+++ PersonalWebsiteDashboard/Website/wwwroot/js/CollapsiblePanel.js
@@ -8,12 +8,14 @@ function togglePanel(panelName) {
         glyphIcon.classList.remove('glyphicon-plus');
         glyphIcon.classList.add('glyphicon-remove');
         panelElement.style.height = "30%";       
-        panelElement.style.overflowY = 'scroll';
+        panelElement.style.overflowY = 'scroll';    
+        panelElement.style.top = '70%';
     }
     else {
         glyphIcon.classList.remove('glyphicon-remove');
         glyphIcon.classList.add('glyphicon-plus');
         panelElement.style.height = "33px";
         panelElement.style.overflow = 'hidden';
+        panelElement.style.top = '96.5%';
     }
 }
--- PersonalWebsiteDashboard/Website/wwwroot/js/p5.js Widgets/GaugeWidget.js
+++ PersonalWebsiteDashboard/Website/wwwroot/js/p5.js Widgets/GaugeWidget.js
@@ -12,7 +12,7 @@ let pctProgress = 0.0;
 let percentTotal = (3.1 / 100) * pct;
 
 // Update Amount
-let speed = 0.04;
+let speed = 0.02;
 
 // Radius of Circle
 let r = 33;
@@ -37,8 +37,8 @@ function draw() {
         y = -r * sin(pctProgress) + 50;
     }
 
-    fill("blue");
-    stroke("blue");
+    fill("#5551FA");
+    stroke("#5551FA");
     strokeWeight(5);
     smooth();
     circle(x, y, 20, 20);
@@ -49,10 +49,11 @@ function drawEmptyCurvedTube() {
     //background(211);
 
     // Semi Circle
-    noFill();
-    stroke("blue");
+    fill("#4F4F4F");
+    noStroke();
     circle(50, 50, 90, 70);
 
     // Hole in Semi Circle
+    fill("#2E3442");
     circle(50, 50, 45, 35);
 }
